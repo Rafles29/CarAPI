c6c40f042b50623856014e2ba73726c6f78de081
Rafles29 <rwozniak@intelgraf.com.pl>
2017-01-23 16:54:48 +0100

    added responses and PUT car

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import json
-from flask import Flask, request, jsonify, Response
+from flask import Flask, request, jsonify, make_response
 import sqlite3
 
 app = Flask(__name__)
@@ -14,7 +14,6 @@ def get_token(login):
     db.close()
     return token
 
-
 def get_car(id):
     db = sqlite3.connect(Database)
     cursor = db.execute("SELECT * FROM CARS WHERE ID = '{}'".format(id))
@@ -23,11 +22,20 @@ def get_car(id):
         car = row
     return jsonify(car_to_json(car))
 
+def change_quantity(id, nmb):
+    db = sqlite3.connect(Database)
+    cursor = db.execute("SELECT QUANTITY FROM CARS WHERE ID = '{}'".format(id))
+    quantity = 0
+    for row in cursor:
+        quantity = row[0]
+    db.execute("UPDATE CARS SET QUANTITY = {} WHERE ID = '{}'".format(quantity+nmb, id))
+    db.commit()
+    db.close()
+    return get_car(id)
 
 def car_to_json(car):
-    return {"ID": car[0], "MANUFACTOR": car[1], "MODEL": car[2], "YEAR": car[3], "FUEL": car[4], "AC": car[5],
-                    "SHORT_PRICE": car[6], "MEDIUM_PRICE": car[7], "LONG_PRICE": car[8]}
-
+    return {"ID": car[0], "MANUFACTOR": car[1], "MODEL": car[2], "YEAR": car[3], "FUEL": car[4], "TRANSMISSION": car[5],
+            "AC": car[6], "SHORT_PRICE": car[7], "MEDIUM_PRICE": car[8], "LONG_PRICE": car[9], "QUANTITY": car[10]}
 
 def get_cars():
     db = sqlite3.connect(Database)
@@ -37,14 +45,14 @@ def get_cars():
         cars.append(car_to_json(car))
     return jsonify(cars)
 
-
 def upload_car(manu, model, year, fuel, transmission, ac, short_price, medium_price, long_price, quantity):
     db = sqlite3.connect(Database)
-    db.execute("INSERT INTO CARS (MANUFACTOR, MODEl, YEAR, FUEL, TRANSMISSION, AC, SHORT_PRICE, MEDIUM_PRICE, LONG_PRICE, QUANTITY) \
+    cursor = db.cursor()
+    cursor.execute("INSERT INTO CARS (MANUFACTOR, MODEl, YEAR, FUEL, TRANSMISSION, AC, SHORT_PRICE, MEDIUM_PRICE, LONG_PRICE, QUANTITY) \
 VALUES('{}','{}',{},'{}','{}','{}',{},{},{},{})".format(manu, model, year, fuel, transmission, ac, short_price, medium_price, long_price, quantity))
     db.commit()
     db.close()
-
+    return get_car(cursor.lastrowid)
 
 def acc_match(login, password):
     db = sqlite3.connect(Database)
@@ -63,11 +71,9 @@ def token_match(token):
     db.close()
     return False
 
-
 @app.route('/')
 def hello_world():
     return 'Hello World!'
-
 
 @app.route('/login', methods=['POST'])
 def login():
@@ -78,8 +84,7 @@ def login():
         if acc_match(login, password):
             return jsonify({"token": get_token(login)})
         else:
-            return 'BAD!'
-
+            return make_response(jsonify({"error": "Invalid login or password"}), 400)
 
 
 @app.route('/cars', methods=['GET', 'POST'])
@@ -89,21 +94,23 @@ def cars():
     elif request.method == 'POST':
         req = request.json
         token = req['token']
-        print(req)
         if(token_match(token)):
-            upload_car(req['manufactor'], req['model'], req['year'], req['fuel'], req['transmission'], req['ac'], req['short_price'], req['medium_price'], req['long_price'], req['quantity'])
-            return jsonify({"message": "OK"})
+            return upload_car(req['manufactor'], req['model'], req['year'], req['fuel'], req['transmission'], req['ac'], req['short_price'], req['medium_price'], req['long_price'], req['quantity'])
         else:
-            return 'nope'
-
+            return make_response(jsonify({"error": "Invalid token"}), 401)
 
 @app.route('/cars/<string:id>', methods=['GET', 'PUT'])
 def car(id):
     if request.method == 'GET':
         return get_car(id)
     elif request.method == 'PUT':
-        return 'PutCar! {}'.format(id)
-
+        req = request.json
+        token = req['token']
+        if (token_match(token)):
+            print(req['nmb'])
+            return change_quantity(id, req['nmb'])
+        else:
+            return make_response(jsonify({"error": "Invalid token"}), 401)
 
 @app.route('/reservations', methods=['GET', "POST"])
 def reservations():
@@ -112,13 +119,13 @@ def reservations():
     elif request.method == 'POST':
         return 'PostReservations!'
 
-
 @app.route('/reservations/<id>', methods=['GET', 'DELETE'])
 def show_reservation(id):
     if request.method == 'GET':
         return 'GetReservation! {}'.format(id)
     elif request.method == 'DELETE':
         return 'DeleteReservation! {}'.format(id)
+
 def main():
     print('This is main')
 
