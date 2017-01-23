48124c67ee4f74ca39af990f0106fc6330c40913
Rafles29 <rwozniak@intelgraf.com.pl>
2017-01-23 22:58:05 +0100

    addded reservations handlers

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import sqlite3
 app = Flask(__name__)
 Database = 'CarData.db'
 
+
 def get_token(login):
     db = sqlite3.connect(Database)
     cursor = db.execute("SELECT TOKEN FROM USERS WHERE LOGIN = '{}'".format(login))
@@ -14,6 +15,7 @@ def get_token(login):
     db.close()
     return token
 
+
 def get_car(id):
     db = sqlite3.connect(Database)
     cursor = db.execute("SELECT * FROM CARS WHERE ID = '{}'".format(id))
@@ -21,6 +23,16 @@ def get_car(id):
     for row in cursor:
         car = row
     return jsonify(car_to_json(car))
+
+
+def get_reservation(id):
+    db = sqlite3.connect(Database)
+    cursor = db.execute("SELECT * FROM RESERVATIONS WHERE ID = '{}'".format(id))
+    reservation =[]
+    for row in cursor:
+        reservation = row
+    return jsonify(reservation_to_json(reservation))
+
 
 def change_quantity(id, nmb):
     db = sqlite3.connect(Database)
@@ -33,9 +45,15 @@ def change_quantity(id, nmb):
     db.close()
     return get_car(id)
 
+
 def car_to_json(car):
     return {"ID": car[0], "MANUFACTOR": car[1], "MODEL": car[2], "YEAR": car[3], "FUEL": car[4], "TRANSMISSION": car[5],
             "AC": car[6], "SHORT_PRICE": car[7], "MEDIUM_PRICE": car[8], "LONG_PRICE": car[9], "QUANTITY": car[10]}
+
+
+def reservation_to_json(reservation):
+    return {"ID": reservation[0], "DATE": reservation[1], "CAR_ID": reservation[2], "CLIENT_ID": reservation[3]}
+
 
 def get_cars():
     db = sqlite3.connect(Database)
@@ -44,6 +62,16 @@ def get_cars():
     for car in cursor:
         cars.append(car_to_json(car))
     return jsonify(cars)
+
+
+def get_reservations():
+    db = sqlite3.connect(Database)
+    cursor = db.execute("SELECT * FROM RESERVATIONS")
+    reservations = []
+    for reservation in cursor:
+        cars.append(car_to_json(reservation))
+    return jsonify(reservations)
+
 
 def upload_car(manu, model, year, fuel, transmission, ac, short_price, medium_price, long_price, quantity):
     db = sqlite3.connect(Database)
@@ -54,6 +82,17 @@ VALUES('{}','{}',{},'{}','{}','{}',{},{},{},{})".format(manu, model, year, fuel,
     db.close()
     return get_car(cursor.lastrowid)
 
+
+def make_reservation(date, car_id, client_id):
+    db = sqlite3.connect(Database)
+    cursor = db.cursor()
+    cursor.execute('''INSERT INTO RESERVATIONS(DATE, CAR_ID, CLIENT_ID)
+    VALUES ('{}', {}, {})'''.format(date, car_id, client_id))
+    db.commit()
+    db.close()
+    return get_reservation(cursor.lastrowid)
+
+
 def delete_car(id):
     db = sqlite3.connect(Database)
     car = get_car(id)
@@ -61,6 +100,16 @@ def delete_car(id):
     db.commit()
     db.close()
     return car
+
+
+def delete_reservation(id):
+    db = sqlite3.connect(Database)
+    reservation = get_reservation(id)
+    db.execute("DELETE FROM RESERVATIONS WHERE ID = {}".format(id))
+    db.commit()
+    db.close()
+    return reservation
+
 
 def acc_match(login, password):
     db = sqlite3.connect(Database)
@@ -71,6 +120,7 @@ def acc_match(login, password):
     db.close()
     return False
 
+
 def token_match(token):
     db = sqlite3.connect(Database)
     cursor = db.execute("SELECT * FROM USERS WHERE TOKEN = '{}'".format(token))
@@ -79,9 +129,11 @@ def token_match(token):
     db.close()
     return False
 
+
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
 
 @app.route('/login', methods=['POST'])
 def login():
@@ -133,19 +185,32 @@ def reservations():
     if request.method == 'GET':
         return 'GetReservations!'
     elif request.method == 'POST':
-        return 'PostReservations!'
+        req = request.json
+        token = req['token']
+        if(token_match(token)):
+            return make_reservation(req['date'], req['car_id'], req['client_id'])
+        else:
+            return make_response(jsonify({"error": "Invalid token"}), 401)
+
 
 @app.route('/reservations/<id>', methods=['GET', 'DELETE'])
 def show_reservation(id):
     if request.method == 'GET':
         return 'GetReservation! {}'.format(id)
     elif request.method == 'DELETE':
-        return 'DeleteReservation! {}'.format(id)
+        req = request.json
+        token = req['token']
+        if (token_match(token)):
+            return delete_reservation(id)
+        else:
+            return make_response(jsonify({"error": "Invalid token"}), 401)
+
 
 def main():
     print('This is main')
     db = sqlite3.connect('CarData.db')
     db.close()
+
 
 if __name__ == '__main__':
     #main()
