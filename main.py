735d9bdead7812f8b2ef3c32421be50c9e1c29af
Rafles29 <rwozniak@intelgraf.com.pl>
2017-01-25 23:38:39 +0100

    added clients control

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ def get_car(id):
     car =[]
     for row in cursor:
         car = row
+    db.close()
     return jsonify(car_to_json(car))
 
 
@@ -31,7 +32,19 @@ def get_reservation(id):
     reservation =[]
     for row in cursor:
         reservation = row
+    db.close()
     return jsonify(reservation_to_json(reservation))
+
+
+def get_client(id):
+    db = sqlite3.connect(Database)
+    cursor = db.execute("SELECT * FROM CLIENTS WHERE ID = '{}'".format(id))
+    client = []
+
+    for row in cursor:
+        client = row
+    db.close()
+    return jsonify(client_to_json(client))
 
 
 def change_quantity(id, nmb):
@@ -55,12 +68,17 @@ def reservation_to_json(reservation):
     return {"ID": reservation[0], "DATE": reservation[1], "CAR_ID": reservation[2], "CLIENT_ID": reservation[3]}
 
 
+def client_to_json(client):
+    return {"ID": client[0], "FIRST_NAME": client[1], "LAST_NAME": client[2], "EMAIL": client[3], "PHONE": client[4], "CITY": client[5]}
+
+
 def get_cars():
     db = sqlite3.connect(Database)
     cursor = db.execute("SELECT * FROM CARS")
     cars = []
     for car in cursor:
         cars.append(car_to_json(car))
+    db.close()
     return jsonify(cars)
 
 
@@ -70,7 +88,18 @@ def get_reservations():
     reservations = []
     for reservation in cursor:
         reservations.append(reservation_to_json(reservation))
+    db.close()
     return jsonify(reservations)
+
+
+def get_clients():
+    db = sqlite3.connect(Database)
+    cursor = db.execute("SELECT * FROM CLIENTS")
+    clients = []
+    for client in cursor:
+        clients.append(client_to_json(client))
+    db.close()
+    return jsonify(clients)
 
 
 def upload_car(manu, model, year, fuel, transmission, ac, short_price, medium_price, long_price, quantity):
@@ -91,6 +120,16 @@ def make_reservation(date, car_id, client_id):
     db.commit()
     db.close()
     return get_reservation(cursor.lastrowid)
+
+
+def add_client(first_name, last_name, email, phone, city):
+    db = sqlite3.connect(Database)
+    cursor = db.cursor()
+    cursor.execute('''INSERT INTO CLIENTS(FIRST_NAME, LAST_NAME, EMAIL, PHONE, CITY)
+    VALUES ('{}', '{}', '{}', '{}', '{}')'''.format(first_name, last_name, email, phone, city))
+    db.commit()
+    db.close()
+    return get_client(cursor.lastrowid)
 
 
 def delete_car(id):
@@ -132,13 +171,17 @@ def token_match(token):
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'hi'
 
 
 @app.route('/login', methods=['POST'])
 def login():
     if request.method == 'POST':
         user = request.get_json()
+        params = ('login', 'password')
+        for param in params:
+            if param not in request.json:
+                return make_response(jsonify({"error": "bad {}".format(param)}), 401)
         login = user['login']
         password = user['password']
         if acc_match(login, password):
@@ -152,70 +195,141 @@ def cars():
     if request.method == 'GET':
         return get_cars()
     elif request.method == 'POST':
-        req = request.json
-        token = request.headers['token']
-        if(token_match(token)):
-            return upload_car(req['manufactor'], req['model'], req['year'], req['fuel'], req['transmission'], req['ac'], req['short_price'], req['medium_price'], req['long_price'], req['quantity'])
+        if 'token' in request.headers:
+            req = request.json
+            token = request.headers['token']
+            if(token_match(token)):
+                params = ('manufactor', 'model', 'year', 'fuel', 'transmission', 'ac', 'short_price', 'medium_price', 'long_price', 'quantity')
+                for param in params:
+                    if param not in req:
+                        return make_response(jsonify({"error": "bad {}".format(param)}), 401)
+                return upload_car(req[params[0]], req[params[1]], req[params[2]], req[params[3]], req[params[4]], req[params[5]], req[params[6]], req[params[7]], req[params[8]], req[params[9]])
+            else:
+                return make_response(jsonify({"error": "Invalid token"}), 401)
         else:
-            return make_response(jsonify({"error": "Invalid token"}), 401)
+            return make_response(jsonify({"error": "token not found"}), 401)
+
 
 @app.route('/cars/<string:id>', methods=['GET', 'PUT', 'DELETE'])
 def car(id):
     if request.method == 'GET':
         return get_car(id)
     elif request.method == 'PUT':
-        req = request.json
-        token = request.headers['token']
-        if (token_match(token)):
-            print(req['nmb'])
-            return change_quantity(id, req['nmb'])
+        if 'token' in request.headers:
+            req = request.json
+            token = request.headers['token']
+            if (token_match(token)):
+                param = 'nmb'
+                if param not in req:
+                    return make_response(jsonify({"error": "bad {}".format(param)}), 401)
+                return change_quantity(id, req[param])
+            else:
+                return make_response(jsonify({"error": "Invalid token"}), 401)
         else:
-            return make_response(jsonify({"error": "Invalid token"}), 401)
+            return make_response(jsonify({"error": "token not found"}), 401)
     elif request.method == 'DELETE':
-        token = request.headers['token']
-        if (token_match(token)):
-            return delete_car(id)
+        if 'token' in request.headers:
+            token = request.headers['token']
+            if (token_match(token)):
+                return delete_car(id)
+            else:
+                return make_response(jsonify({"error": "Invalid token"}), 401)
         else:
-            return make_response(jsonify({"error": "Invalid token"}), 401)
+            return make_response(jsonify({"error": "token not found"}), 401)
 
 
 @app.route('/reservations', methods=['GET', "POST"])
 def reservations():
     if request.method == 'GET':
-        token = request.headers['token']
-        if(token_match(token)):
-            return get_reservations()
+        if 'token' in request.headers:
+            token = request.headers['token']
+            if(token_match(token)):
+                return get_reservations()
+            else:
+                return make_response(jsonify({"error": "Invalid token"}), 401)
         else:
-            return make_response(jsonify({"error": "Invalid token"}), 401)
+            return make_response(jsonify({"error": "token not found"}), 401)
     elif request.method == 'POST':
-        req = request.json
-        token = request.headers['token']
-        if(token_match(token)):
-            return make_reservation(req['date'], req['car_id'], req['client_id'])
+        if 'token' in request.headers:
+            req = request.json
+            token = request.headers['token']
+            if(token_match(token)):
+                params = ('date', 'car_id', 'client_id')
+                for param in params:
+                    if param not in req:
+                        return make_response(jsonify({"error": "bad {}".format(param)}), 401)
+                return make_reservation(req[params[0]], req[params[1]], req[params[2]])
+
+            else:
+                return make_response(jsonify({"error": "Invalid token"}), 401)
         else:
-            return make_response(jsonify({"error": "Invalid token"}), 401)
-
-
+            return make_response(jsonify({"error": "token not found"}), 401)
 @app.route('/reservations/<id>', methods=['GET', 'DELETE'])
 def show_reservation(id):
     if request.method == 'GET':
-        token = request.headers['token']
-        if(token_match(token)):
-            return get_reservation(id)
+        if 'token' in request.headers:
+            token = request.headers['token']
+            if(token_match(token)):
+                return get_reservation(id)
+            else:
+                return make_response(jsonify({"error": "Invalid token"}), 401)
         else:
-            return make_response(jsonify({"error": "Invalid token"}), 401)
+            return make_response(jsonify({"error": "token not found"}), 401)
     elif request.method == 'DELETE':
-        token = request.headers['token']
-        if (token_match(token)):
-            return delete_reservation(id)
+        if 'token' in request.headers:
+            token = request.headers['token']
+            if (token_match(token)):
+                return delete_reservation(id)
+            else:
+                return make_response(jsonify({"error": "Invalid token"}), 401)
         else:
-            return make_response(jsonify({"error": "Invalid token"}), 401)
+            return make_response(jsonify({"error": "token not found"}), 401)
+
+
+@app.route('/clients', methods=['GET', "POST"])
+def clients():
+    if request.method == 'GET':
+        if 'token' in request.headers:
+            token = request.headers['token']
+            if(token_match(token)):
+                return get_clients()
+            else:
+                return make_response(jsonify({"error": "Invalid token"}), 401)
+        else:
+            return make_response(jsonify({"error": "token not found"}), 401)
+    elif request.method == 'POST':
+        if 'token' in request.headers:
+            req = request.json
+            token = request.headers['token']
+            if(token_match(token)):
+                params = ('first_name', 'last_name', 'email', 'phone', 'city')
+                for param in params:
+                    if param not in req:
+                        return make_response(jsonify({"error": "bad {}".format(param)}), 401)
+                return add_client(req[params[0]], req[params[1]], req[params[2]], req[params[3]], req[params[4]])
+
+            else:
+                return make_response(jsonify({"error": "Invalid token"}), 401)
+        else:
+            return make_response(jsonify({"error": "token not found"}), 401)
+
+
+@app.route('/clients/<id>', methods=['GET'])
+def client(id):
+    if request.method == 'GET':
+        if 'token' in request.headers:
+            token = request.headers['token']
+            if(token_match(token)):
+                return get_client(id)
+            else:
+                return make_response(jsonify({"error": "Invalid token"}), 401)
+        else:
+            return make_response(jsonify({"error": "token not found"}), 401)
+
 
 
 def main():
     print('This is main')
-    db = sqlite3.connect('CarData.db')
-    db.close()
 
 
 if __name__ == '__main__':
